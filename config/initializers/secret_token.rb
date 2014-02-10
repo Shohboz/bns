8accd4845d3836157ddb9eb70b4e83bd1a0a6e08
Shohboz <vip.shohboz@gmail.com>
2014-02-11 03:12:13 +0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BnservicePromo::Application.config.secret_key_base = '64f15c1c174551b9f94c5f8c4f64384740c5708fb78cca100c188be861f7ea8ecd4fb946d9396168412137be42d67001f22c6e9f1532259e689dcc13542fc156'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BnservicePromo::Application.config.session_store :cookie_store, key: '_bnservice_promo_session'
